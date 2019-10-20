f2a9eb47b7dde12bb0f3ef92553dcee07afaa23e
Noob <ExplodingFKL@gmail.com>
2019-10-20 21:28:13 +0800

    create new project.

---
+++ cli/src/main/java/top/fksoft/remote/Main.kt
@@ -0,0 +1,15 @@
+package top.fksoft.remote
+
+import jdkUtils.logcat.Logger
+
+/**
+ * @author Explo
+ */
+
+object Main {
+    private val logger = Logger.getLogger(this)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        logger.info("Hello World!")
+    }
+}
